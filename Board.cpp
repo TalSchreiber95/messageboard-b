f19d8b3ebbe783afd7887dedf52c9b66ce814706
talSchreiber95 <talfreestyle@gmail.com>
2021-03-25 20:52:36 +0200

    final submission

---
+++ Board.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include "Board.hpp"
+/*
+    Board.cpp.
+    Author: Tell Schreiber.
+*/
+using namespace std;
+
+namespace ariel {
+ void Board::post(uint row, uint col, Direction direction, string message) 
+ {
+        uint len=message.length();
+        if(this->flag){
+            this->flag=false;
+            this->min_col=col;
+            this->min_row=row;
+        }
+        if(direction==Direction::Horizontal){
+            this->max_row = max(this->max_row, row);
+            this->max_col = max(this->max_col, col+len);
+        }
+        else{
+            this->max_row = max(this->max_row, row+len);
+            this->max_col = max(this->max_col, col);
+        }
+
+        this->min_row = min(this->min_row, row);
+        this->min_col = min(this->min_col, col);
+        for(ulong i=0 ;i<len;i++){
+            this->board[row][col].v = message[i];
+            if(direction == Direction::Horizontal){
+                col++;
+            }
+            else{
+                row++;
+            } 
+        }
+    }
+
+    string Board::read(uint row, uint col, Direction direction, uint length){
+        string msgBoard;
+        for(uint i=0; i<length; i++){
+            msgBoard += this->board[row][col].v;
+            if(direction == Direction::Horizontal){
+                col++;
+            }
+            else{
+                row++;
+            } 
+        }
+        return msgBoard;
+    }
+    
+    void Board::show() {
+        for (uint i = this->min_row; i < this->max_row; i++) {
+            cout << i<< ": ";
+            for (uint j = this->min_col; j < this->max_col; j++) {
+                cout << this->board[i][j].v ;
+            }
+            cout << "\n";
+        }
+    }
+}
---
+++ Board.hpp
@@ -0,0 +1,37 @@
+#include <string>
+// #include <vector>
+#include <map>
+#include "Direction.hpp"
+/*
+    Board.hpp.
+    Author: Tal Schreiber.
+*/
+using namespace std;
+
+struct defChar{
+    char v='_'; // this is the default value of each character types in our board
+};
+
+namespace ariel {
+    class Board {
+        map<uint,map<uint,defChar>> board;
+        bool flag;
+        uint min_col;
+        uint min_row; 
+        uint max_col;
+        uint max_row;
+    public:
+        Board() {
+            flag=true;// Created due to issues with INT16_MAX value that is 127 insted of MaxInt that should be.
+            min_col=0;//INT16_MAX;
+            min_row=0;//INT16_MAX; 
+            max_col=0;
+            max_row=0;
+        }
+        ~Board(){}; // class's destructor
+        void post(uint row, uint col, Direction direction, std::string message);
+        std::string read(uint row, uint col, Direction direction, uint length);
+        void show();
+    };
+
+}
